3772799386508200d5fff0ef31d045e15da503f1
Kevin Li <kevin@Kevins-MacBook-Pro-4.local>
2024-01-30 16:13:44 -0600

    display name

--- test/test.ino
+++ test/test.ino
@@ -1,10 +1,15 @@
 #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   // put your setup code here, to run once:
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  
+  lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
+  lcd.print("My name is:");
+  lcd.setCursor(0, 1);
+  lcd.print("123");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50); //Wait 50 ms
 }
